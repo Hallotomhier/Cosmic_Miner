0bddf5c7080eb477caba69f9eb65d90f18e8ffde
Hallotomhier <tomwinkel2005@gmail.com>
2023-05-15 13:21:35 +0200

    Nieuwe Hp En O2 Script

---
+++ Assets/Script/Health/AirSystem.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirSystem : MonoBehaviour
+{
+    public Transform player;
+    public bool inside_Air;
+   
+
+
+    public void OnTriggerStay(Collider player)
+    {
+        inside_Air = true;
+    }
+
+    public void OnTriggerExit(Collider player)
+    {
+        inside_Air = false;
+    }
+}
---
+++ Assets/Script/Health/hp_O2.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class hp_O2 : MonoBehaviour
+{
+    [Range(0,100)]public float oxygen = 100f;
+    [Range(0,100)]public float health = 100f;
+    public Slider oxygen_slider;
+    public Slider health_slider;
+    public Vector3 respawn_pos;
+    public AirSystem airsystem;
+    public bool insideair;
+
+    
+    void Update()
+    {
+        insideair = airsystem.inside_Air;
+        Hp();
+        O2();
+        oxygen_slider.value = oxygen;
+        health_slider.value = health;
+        
+    }
+
+    public void O2() 
+    {
+        if(insideair == false) 
+        {
+            oxygen -= 0.5f * Time.deltaTime;
+            
+        
+        }
+    
+    }
+
+    public void Hp() 
+    {
+        if(oxygen == 0)  
+        {
+            health -= 2f * Time.deltaTime;
+            
+        } 
+        if(health== 0) 
+        {
+            Debug.Log("die");
+            //transform.position = respawn_pos;
+        }
+    
+    }
+}
