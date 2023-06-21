7d7539c2f75b7e9b9c1969cd734e8b9fc6c1bd1b
JelmerHofstee <133031977+JelmerHofstee2@users.noreply.github.com>
2023-06-21 16:42:35 +0200

    Main menu

--- Assets/Script/MenuScripts/ScreenRes.cs
+++ Assets/Script/MenuScripts/ScreenRes.cs
@@ -9,8 +9,8 @@ public class ScreenRes : MonoBehaviour
     public GameObject fsOn;
     // public GameObject dropDown;
     public int dropDownValue;
-    public Dropdown dropDown;
-    public GameObject label;
+    // public Dropdown dropDown;
+    // public GameObject label;
    
     // Start is called before the first frame update
     void Start()
@@ -18,7 +18,7 @@ public class ScreenRes : MonoBehaviour
         Screen.SetResolution(1920, 1080, FullScreenMode.Windowed);
         // Screen.fullScreen = false;
         fsOn.GetComponent<Toggle>().isOn = false;
-        dropDown = GetComponent<Dropdown>();
+        // dropDown = GetComponent<Dropdown>();
         // Debug.Log(dropDown.value);
         // Debug.Log(EventSystem.current.currentSelectedGameObject.name);
     }
@@ -31,36 +31,36 @@ public class ScreenRes : MonoBehaviour
         Screen.fullScreen = fsOn;
         Debug.Log("fullscreen is " + fsOn);
     }
-    public void ResolutionChange()
-    {
-        if(dropDown.GetComponent<Dropdown>().value == 0)
-        {
-            Screen.SetResolution(720, 480, FullScreenMode.Windowed);
-            fsOn.GetComponent<Toggle>().isOn = false;
-            Debug.Log("hey0");
-        }
-        if(dropDown.GetComponent<Dropdown>().value == 1)
-        {
-            Screen.SetResolution(1280, 720, FullScreenMode.Windowed);
-            fsOn.GetComponent<Toggle>().isOn = false;       
-        }
-                if(dropDown.GetComponent<Dropdown>().value == 2)
-        {
-            Screen.SetResolution(1920, 1080, FullScreenMode.Windowed);
-            fsOn.GetComponent<Toggle>().isOn = false;       
-        }
-        if(dropDown.GetComponent<Dropdown>().value == 3)
-        {
-            Screen.SetResolution(2560, 1440, FullScreenMode.Windowed);
-            fsOn.GetComponent<Toggle>().isOn = false;  
-        }
+    // public void ResolutionChange()
+    // {
+    //     if(dropDown.GetComponent<Dropdown>().value == 0)
+    //     {
+    //         Screen.SetResolution(720, 480, FullScreenMode.Windowed);
+    //         fsOn.GetComponent<Toggle>().isOn = false;
+    //         Debug.Log("hey0");
+    //     }
+    //     if(dropDown.GetComponent<Dropdown>().value == 1)
+    //     {
+    //         Screen.SetResolution(1280, 720, FullScreenMode.Windowed);
+    //         fsOn.GetComponent<Toggle>().isOn = false;       
+    //     }
+    //             if(dropDown.GetComponent<Dropdown>().value == 2)
+    //     {
+    //         Screen.SetResolution(1920, 1080, FullScreenMode.Windowed);
+    //         fsOn.GetComponent<Toggle>().isOn = false;       
+    //     }
+    //     if(dropDown.GetComponent<Dropdown>().value == 3)
+    //     {
+    //         Screen.SetResolution(2560, 1440, FullScreenMode.Windowed);
+    //         fsOn.GetComponent<Toggle>().isOn = false;  
+    //     }
         
-        // if(label.GetComponent<>() == "1920 x 1080")
-        // {
-            //  Debug.Log("HEY");
-        // }
+    //     // if(label.GetComponent<>() == "1920 x 1080")
+    //     // {
+    //         //  Debug.Log("HEY");
+    //     // }
         
-    }
+    // }
     public void Resolution1()
     {
         Screen.SetResolution(720, 480, FullScreenMode.Windowed);
