24c48acf29f6f004e7ecad5f16d0960fb6e8fabe
JelmerHofstee <133031977+JelmerHofstee2@users.noreply.github.com>
2023-07-03 13:28:59 +0200

    Onhover

---
+++ Assets/Script/MenuScripts/OnHover.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class OnHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public bool mouse_over = false;
+    public string dataPointer;
+    public GameObject buildingInfoObject;
+    public TMP_Text buildingInfo;
+
+    void Update()
+    {
+        if (mouse_over)
+        {
+            buildingInfoObject.SetActive(true);
+            buildingInfo.text = dataPointer;
+            Debug.Log("Mouse Over");
+        }
+        else
+        {
+            buildingInfoObject.SetActive(false);
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        mouse_over = true;
+        // if(eventData.pointerCurrentRaycast.gameObject)
+        // {
+        //     dataPointer = eventData.pointerCurrentRaycast.gameObject.name;
+        // }
+        
+
+        // Debug.Log("Mouse enter");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        mouse_over = false;
+        Debug.Log("Mouse exit");
+    }
+}
